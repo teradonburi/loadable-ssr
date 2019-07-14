e4d024c0906056c73cef6f8868d6a43ff2770b28
teradonburi <daikiterai@gmail.com>
2019-07-14 09:16:44 +0900

    refactor

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const LoadablePlugin = require('@loadable/webpack-plugin')
+
+const DIST_PATH = path.resolve(__dirname, 'public/dist')
+const production = process.env.NODE_ENV === 'production'
+const development =
+  !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
+const getConfig = target => ({
+  name: target,
+  mode: development ? 'development' : 'production',
+  target,
+  entry: `./src/client/main-${target}.js`,
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            caller: { target },
+          },
+        },
+      },
+    ],
+  },
+  externals:
+    target === 'node' ? ['@loadable/component', nodeExternals()] : undefined,
+  output: {
+    path: path.join(DIST_PATH, target),
+    filename: production ? '[name]-bundle-[chunkhash:8].js' : '[name].js',
+    publicPath: `/dist/${target}/`,
+    libraryTarget: target === 'node' ? 'commonjs2' : undefined,
+  },
+  plugins: [new LoadablePlugin()],
+})
+
+export default [getConfig('web'), getConfig('node')]
